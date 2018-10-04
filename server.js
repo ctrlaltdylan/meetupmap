a7c5a369bcf2b4582a94f9370170c7d404f50e25
Dylan Pierce <dylanpierce914@gmail.com>
2018-10-03 17:12:04 -0700

    Using Azure's port

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
-const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+const port = 8080 || process.env.PORT;
 const request = require('request');
 const cors = require('cors');
 const qs = require('querystring');
